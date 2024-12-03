fcfcbbe27c1e70ee5c386075aaa7d5e3c808003a
StarRainArt <c.verhaar03@gmail.com>
2024-12-03 09:53:50 +0100

    everything works again jezus christ

--- AllergEase/loginpages/inloggen.js
+++ AllergEase/loginpages/inloggen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TextInput, Button, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
