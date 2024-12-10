6ab70571eed8148e9e03aec3d61e730cc34932ea
6031920 <6031920@mborijnland.nl>
2024-12-10 13:34:14 +0100

    Recepten worden opgehaalt en getoont. Styling, filteren, ect nog niet werkend

--- AllergEase/recipes/recipes.js
+++ AllergEase/recipes/recipes.js
@@ -2,61 +2,80 @@ import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, StyleSheet, Image } from "react-native";
 
 const SPOONACULAR_API_KEY = '20d4411eec5744e79d7906bea977857e';
-const COMPLEXSEARCH = 'https://api.spoonacular.com/recipes/complexSearch';
+const SEARCH = 'https://api.spoonacular.com/recipes/';
 
 
 
 
 
-export default function RecipesPage(query = null) {
+export default function RecipesPage({query = null}) {
+  const recipesPerPage = 10;
+  const maxPages = 2;
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [isFetchingMore, setIsFetchingMore] = useState(false);
+
   useEffect(() => {
-    const getRecipesSearch = async (query = null) => {
+    const getRecipesSearch = async (query = null, page = 1) => {
       try {
         let url;
         if (query == null) {
-          url = `${COMPLEXSEARCH}?number=2&apiKey=${SPOONACULAR_API_KEY}`
+          url = `${SEARCH}/complexSearch?number=${recipesPerPage}&apiKey=${SPOONACULAR_API_KEY}`
         } else {
-          url = `${COMPLEXSEARCH}?number=20&query=${query}&apiKey=${SPOONACULAR_API_KEY}`
+          url = `${SEARCH}complexSearch?number=${recipesPerPage}&offset=${(page - 1) * recipesPerPage}&query=${query}&apiKey=${SPOONACULAR_API_KEY}`
         }
-        const response = await fetch(url)
-
-        const json = await response.json()
-        setData(json)
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        setData(prevData => [...prevData, ...json.results]);
       }
       catch (error) {
         console.error("Failed to fetch recipe details:", error);
       }
       finally {
-        setLoading(false)
+        setLoading(false);
+        setIsFetchingMore(false);
       }
     }
-    getRecipesSearch();
-  }, []);
-  const RenderRecipe = ({ title }) => {
+    getRecipesSearch(query, page);
+  }, [query, page]);
+
+  const loadMoreRecipes = () => {
+    if (!isFetchingMore && page < maxPages) {
+      setIsFetchingMore(true);
+      setPage(prevPage => prevPage + 1);
+    }
+  };
+
+  const RenderRecipe = ({ title, image }) => (
     <View style={recipesStyle.item}>
       <Text style={recipesStyle.title}>{title}</Text>
+      {image && <Image source={{ uri: image }} style={recipesStyle.image} />}
     </View>
-  }
+  );
 
-  if(loading){
+
+  if(loading && page == 1) {
     return <View>
       <Text>loading</Text>
     </View>
   }
-  let results = data.results
   return (
     <View style={recipesStyle.container}>
       <FlatList
-        data={results}
-        renderItem={({item}) => <RenderRecipe title={item.title}/>}
-        keyExtractor={item => item.id}
+        data={data}
+        renderItem={({item}) => <RenderRecipe title={item.title} image={item.image} />}
+        keyExtractor={(item) => item.id.toString()}
+        onEndReached={!query ? null : loadMoreRecipes}
+        onEndReachedThreshold={0.5}
+        ListFooterComponent={isFetchingMore ? <Text>Loading...</Text> : <Text>Filter for more</Text>}
       />
     </View>
   );
 
 }
+// Styling temporary
 const recipesStyle = StyleSheet.create({
   container: {
     flex: 1,
@@ -70,5 +89,9 @@ const recipesStyle = StyleSheet.create({
   },
   title: {
     fontSize: 32,
+  },  
+  image: {
+    width: 100,
+    height: 100,
   },
 });
