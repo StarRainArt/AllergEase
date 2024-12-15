d13c410abed5df753fbcbd7c60404b0388ad55a0
StarRainArt <c.verhaar03@gmail.com>
2024-12-15 16:52:11 +0100

    refixed style.js and removed header fridgepages

--- AllergEase/style.js
+++ AllergEase/style.js
@@ -15,39 +15,42 @@ const styles = StyleSheet.create({
       color: "#472D30",
       paddingVertical: 20
     },
+    kopje: {
+      fontSize: 25,
+      fontFamily: "DynaPuff",
+      color: "#472D30",
+    },
     sectionGreen: {
       backgroundColor: "#C9CBA3",
       color: "#472D30",
       borderRadius: 15,
-      padding: 5
+      paddingVertical: 15,
+      paddingHorizontal: 20
     },
     sectionYellow: {
       backgroundColor: "#FFE1A8",
       color: "#472D30",
       borderRadius: 15,
-      padding: 5
+      paddingVertical: 15,
+      paddingHorizontal: 20
     },
     buttonRed: {
       backgroundColor: "#E26D5C",
-      width: "60%",
       borderRadius: 15,
       paddingVertical: 5
     },
     buttonGreen: {
       backgroundColor: "#C9CBA3",
-      width: "60%",
       borderRadius: 15,
       paddingVertical: 5
     },
     greenButtonText: {
       color: "#472D30",
-      fontSize: 25,
       fontFamily: "DynaPuff",
       textAlign: "center",
     },
     redButtonText: {
       color: "#FFF5E1",
-      fontSize: 25,
       fontFamily: "DynaPuff",
       textAlign: "center",
     },
@@ -63,7 +66,7 @@ const styles = StyleSheet.create({
       fontSize: 25,
       fontFamily: "BalooPaaji2",
       textAlignVertical: "bottom"
-    }
+    },
 });
 
 export default styles;
